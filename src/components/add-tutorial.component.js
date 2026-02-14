0f248e03f5a091ee7b437f23055bbf6db0e1a1bf
Serhii Korniichuk <koorneeyy@gmail.com>
2026-02-14 15:17:54 +0200

    Rewrite Class Component to funtional approach

--- src/components/add-tutorial.component.js
+++ src/components/add-tutorial.component.js
@@ -1,68 +1,43 @@
-//import React, { Component } from "react";
-import React, { useState, useEffect, Component } from 'react';
+import { useState } from 'react';
+import { withRouter } from '../common/with-router';
 
 import TutorialDataService from "../services/tutorial.service";
 import LocationDataService from "../services/location.service";
 
-export default class AddTutorial extends Component {
-  constructor(props) {
-    super(props);
-    this.onChangeTitle = this.onChangeTitle.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.saveTutorial = this.saveTutorial.bind(this);
-    this.newTutorial = this.newTutorial.bind(this);
-
-    this.state = {
+function AddTutorial(props) {
+  const [state, setState] = useState({
       id: null,
       title: "",
-      description: "", 
-      published: false,
-      locations: [],
-
-      submitted: false
-    };
-  }
+      description: "",
+      published: false
+    });
 
   
-  onChangeTitle(e) {
-    this.setState({
-      title: e.target.value
-    });
+  const onChangeTitle = (e) => {
+    const title = e.target.value;
+    setState(prev => ({ ...prev, title }));
   }
 
-  onChangeDescription(e) {
-    // var locs = [];
-    console.log("!!!!", LocationDataService.getAll().then(resp=>{
-      
-      this.setState({
-      locations: resp.data
-    });
-    //      console.log(resp)
-    }))
-    this.setState({
-      description: e.target.value,
-      
-    });
-    console.log(this.state.locations);
-    
+  const onChangeDescription = (e) => {
+    const description = e.target.value;
+    setState(prev => ({ ...prev, description }));
+    // console.log(this.state.locations);  !!!!!!
   }
 
-  saveTutorial() {
+  const saveTutorial = () => {
     var data = {
-      title: this.state.title,
-      description: this.state.description
+      title: state.title,
+      description: state.description
     };
 
     TutorialDataService.create(data)
       .then(response => {
-        this.setState({
-          id: response.data.id,
-          title: response.data.title,
-          description: response.data.description,
-          published: response.data.published,
-
-          submitted: true
-        });
+        const id = response.data.id;
+        const title = response.data.title;
+        const description = response.data.description;
+        const published = response.data.published;
+        const submitted = true;
+        setState(prev => ({...prev, id, title, description, published, submitted}))
         console.log(response.data);
       })
       .catch(e => {
@@ -70,61 +45,60 @@ export default class AddTutorial extends Component {
       });
   }
 
-  newTutorial() {
-    this.setState({
+  const newTutorial = () => {
+    setState(prev => ({
       id: null,
       title: "",
       description: "",
       published: false,
-
       submitted: false
-    });
+    }));
   }
 
-  render() {
-    return (
-      <div className="submit-form">
-        {this.state.submitted ? (
-          <div>
-            <h4>You submitted successfully!</h4>
-            <button className="btn btn-success" onClick={this.newTutorial}>
-              Add
-            </button>
+  return (
+    <div className="submit-form">
+      {state.submitted ? (
+        <div>
+          <h4>You submitted successfully!</h4>
+          <button className="btn btn-success" onClick={newTutorial}>
+            Add
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              required
+              value={state.title}
+              onChange={onChangeTitle}
+              name="title"
+            />
           </div>
-        ) : (
-          <div>
-            <div className="form-group">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                className="form-control"
-                id="title"
-                required
-                value={this.state.title}
-                onChange={this.onChangeTitle}
-                name="title"
-              />
-            </div>
 
-            <div className="form-group">
-              <label htmlFor="description">Description</label>
-              <input
-                type="text"
-                className="form-control"
-                id="description"
-                required
-                value={this.state.description}
-                onChange={this.onChangeDescription}
-                name="description"
-              />
-            </div>
-            
-            <button onClick={this.saveTutorial} className="btn btn-success">
-              Submit
-            </button>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <input
+              type="text"
+              className="form-control"
+              id="description"
+              required
+              value={state.description}
+              onChange={onChangeDescription}
+              name="description"
+            />
           </div>
-        )}
-      </div>
-    );
-  }
+          
+          <button onClick={saveTutorial} className="btn btn-success">
+            Submit
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default withRouter(AddTutorial);
